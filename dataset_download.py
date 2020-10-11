2e12b0e92d42b8124b6ab679fb17dbc74e862b52
zabir-nabil <zabiralnazi@yahoo.com>
2020-10-11 22:50:09 +0600

    added

--- dataset_download.py
+++ dataset_download.py
@@ -1,12 +1,14 @@
 from kaggle.api.kaggle_api_extended import KaggleApi
+import os
+
 api = KaggleApi()
 api.authenticate()
 
 print('authentication done')
 
-# api.competition_download_files('osic-pulmonary-fibrosis-progression')
+# api.competition_download_files('osic-pulmonary-fibrosis-progression') # this may fail
 
-import os
-os.system("kaggle datasets download khoongweihao/osic-model-weights")
+
+os.system("kaggle competitions download -c osic-pulmonary-fibrosis-progression")
 
 print('done')
